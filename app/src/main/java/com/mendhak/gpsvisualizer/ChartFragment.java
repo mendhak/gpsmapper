7b22d65c588eb528c194082fb902002661e17929
mendhak <smaltaf@msn.com>
2014-10-16 21:30:23 +0100

    Added HelloCharts

---
+++ app/src/main/java/com/mendhak/gpsvisualizer/ChartFragment.java
@@ -0,0 +1,141 @@
+package com.mendhak.gpsvisualizer;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.common.collect.Lists;
+import com.google.common.primitives.Booleans;
+import com.google.common.primitives.Ints;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.ValueShape;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.util.Utils;
+import lecho.lib.hellocharts.view.LineChartView;
+
+
+public class ChartFragment extends Fragment{
+
+    private LineChartView chart;
+    private LineChartData data;
+    private int numberOfLines = 1;
+    private int maxNumberOfLines = 4;
+    private int numberOfPoints = 12;
+
+    float[][] randomNumbersTab = new float[maxNumberOfLines][numberOfPoints];
+
+    private boolean hasAxes = true;
+    private boolean hasAxesNames = true;
+    private boolean hasLines = true;
+    private boolean hasPoints = true;
+    private ValueShape shape = ValueShape.CIRCLE;
+    private boolean isFilled = false;
+    private boolean hasLabels = false;
+    private boolean isCubic = false;
+    private boolean hasLabelForSelected = false;
+
+
+    public static ChartFragment newInstance(int sectionNumber) {
+        ChartFragment fragment = new ChartFragment();
+        Bundle args = new Bundle();
+        args.putInt("section_number", sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public ChartFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_chart, container, false);
+
+        chart = (LineChartView)rootView.findViewById(R.id.chart);
+
+        generateValues();
+        generateData();
+
+        // Disable viewpirt recalculations, see toggleCubic() method for more info.
+        chart.setViewportCalculationEnabled(false);
+
+        chart.setValueSelectionEnabled(true);
+
+
+        resetViewport();
+
+        return rootView;
+    }
+
+    private void resetViewport() {
+        // Reset viewport height range to (0,100)
+        final Viewport v = new Viewport(chart.getMaxViewport());
+        v.bottom = 0;
+        v.top = 100;
+        chart.setMaxViewport(v);
+        chart.setCurrentViewport(v, false);
+    }
+
+    private void generateValues() {
+        for (int i = 0; i < maxNumberOfLines; ++i) {
+            for (int j = 0; j < numberOfPoints; ++j) {
+                randomNumbersTab[i][j] = (float) Math.random() * 100f;
+            }
+        }
+    }
+
+    private void generateData() {
+
+        List<Line> lines = Lists.newLinkedList();
+
+        for (int i = 0; i < numberOfLines; ++i) {
+
+            List<PointValue> values = Lists.newLinkedList();
+            for (int j = 0; j < numberOfPoints; ++j) {
+                values.add(new PointValue(j, randomNumbersTab[i][j]));
+            }
+
+            Line line = new Line(values);
+            line.setColor(Utils.COLORS[i]);
+            line.setShape(shape);
+            line.setCubic(isCubic);
+            line.setFilled(isFilled);
+            line.setHasLabels(true);
+            line.setHasLabelsOnlyForSelected(false);
+            line.setHasLines(hasLines);
+            line.setHasPoints(hasPoints);
+            lines.add(line);
+        }
+
+        data = new LineChartData(lines);
+
+        if (hasAxes) {
+            Axis axisX = new Axis();
+            Axis axisY = new Axis().setHasLines(true);
+            if (hasAxesNames) {
+                axisX.setName("Axis X");
+                axisY.setName("Axis Y");
+            }
+            data.setAxisXBottom(axisX);
+            data.setAxisYLeft(axisY);
+        } else {
+            data.setAxisXBottom(null);
+            data.setAxisYLeft(null);
+        }
+
+        data.setBaseValue(Float.NEGATIVE_INFINITY);
+        chart.setLineChartData(data);
+
+    }
+}
---
+++ app/src/main/java/com/mendhak/gpsvisualizer/MapFragment.java
@@ -0,0 +1,143 @@
+package com.mendhak.gpsvisualizer;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+public  class MapFragment extends Fragment {
+
+    public static MapFragment newInstance(int sectionNumber) {
+        MapFragment fragment = new MapFragment();
+        Bundle args = new Bundle();
+        args.putInt("section_number", sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public MapFragment() {
+    }
+
+    MapView mapView;
+    private GoogleMap googleMap;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        // inflat and return the layout
+        View v = inflater.inflate(R.layout.fragment_map, container,
+                false);
+        mapView = (MapView) v.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+        //mapView.requestTransparentRegion(mapView);
+
+        mapView.onResume();// needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        googleMap = mapView.getMap();
+
+
+
+
+
+//            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+//                    new LatLng(-18.142, 178.431), 2),2000,null);
+//
+//            // Polylines are useful for marking paths and routes on the map.
+//            googleMap.addPolyline(new PolylineOptions().geodesic(true)
+//                    .add(new LatLng(-33.866, 151.195))  // Sydney
+//                    .add(new LatLng(-18.142, 178.431))  // Fiji
+//                    .add(new LatLng(21.291, -157.821))  // Hawaii
+//                    .add(new LatLng(37.423, -122.091))  // Mountain View
+//            );
+
+
+        final LatLng SYDNEY = new LatLng(-33.88,151.21);
+        final LatLng MOUNTAIN_VIEW = new LatLng(37.4, -122.1);
+
+        // Move the camera instantly to Sydney with a zoom of 15.
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SYDNEY, 15));
+
+// Zoom in, animating the camera.
+        googleMap.animateCamera(CameraUpdateFactory.zoomIn());
+
+// Zoom out to zoom level 10, animating with a duration of 2 seconds.
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+
+// Construct a CameraPosition focusing on Mountain View and animate the camera to that position.
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(MOUNTAIN_VIEW)      // Sets the center of the map to Mountain View
+                .zoom(17)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+//
+//
+//            // latitude and longitude
+//            double latitude = 51;
+//            double longitude = -2;
+//
+//            // create marker
+//            MarkerOptions marker = new MarkerOptions().position(
+//                    new LatLng(latitude, longitude)).title("Hello Maps");
+//
+//            // Changing marker icon
+//            marker.icon(BitmapDescriptorFactory
+//                    .defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+//
+//            // adding marker
+//            googleMap.addMarker(marker);
+//            CameraPosition cameraPosition = new CameraPosition.Builder()
+//                    .target(new LatLng(51, -2)).zoom(12).build();
+//            googleMap.animateCamera(CameraUpdateFactory
+//                    .newCameraPosition(cameraPosition));
+
+        //googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+
+        // Perform any camera updates here
+        return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+}
