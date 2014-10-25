c9a452c649f97267205f2beeee122ea23b4052f7
mendhak <smaltaf@msn.com>
2014-10-25 09:13:58 +0200

    Renamed menu charts

--- app/src/main/java/com/mendhak/gpsvisualizer/views/ChartFragment.java
+++ app/src/main/java/com/mendhak/gpsvisualizer/views/ChartFragment.java
@@ -276,7 +276,7 @@ public class ChartFragment extends Fragment{
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.line_chart, menu);
+        inflater.inflate(R.menu.charts, menu);
     }
 
     @Override
